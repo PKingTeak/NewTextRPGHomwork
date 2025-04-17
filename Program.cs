0f4c6b19fe180f1e8f274848563a745faf704307
PKingTeak <119428177+PKingTeak@users.noreply.github.com>
2025-04-17 13:59:51 +0900

    NewProject

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿class Unit
+{
+    protected struct UnitState
+    {
+        public int Hp;
+        public int Damage;
+        public int Level;
+        public int MaxEXP;
+    }
+
+    
+    protected UnitState state;
+
+
+}
+
+class Player : Unit
+{
+    public Player(string _name,  int _hp , int Damage)
+    {
+        
+    }
+
+}
+
+
+
+
+class Program
+{
+
+    static void Main(string arg[])
+    {
+      
+
+
+    }
+}
